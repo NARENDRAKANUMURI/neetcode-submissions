6fa83134cec0119bc0885d68bdf52620f49aab95
NARENDRAKANUMURI <119294166+NARENDRAKANUMURI@users.noreply.github.com>
2026-09-13 16:43:21 +0530

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        words = set(wordDict)
+        memo = {}
+
+        def dfs(start):
+            if start == len(s):
+                return [""]
+
+            if start in memo:
+                return memo[start]
+
+            res = []
+
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+
+                if word in words:
+                    for suffix in dfs(end):
+                        if suffix:
+                            res.append(word + " " + suffix)
+                        else:
+                            res.append(word)
+
+            memo[start] = res
+            return res
+
+        return dfs(0)
